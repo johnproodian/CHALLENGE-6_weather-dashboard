3733e094f4fed1859e2788e51209ccf99cf5a412
Jotas <jproodian721@gmail.com>
2021-12-14 23:28:52 -0600

    initial html, css, and some notes for where to go with JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Get previous searches (local storage) fcn --> Check local storage and get previous searches
+// Display previous searches fcn --> Display previous searches in clickable buttons below search form
+
+// Call get local storage and display local storage fcns(2)
+
+// Form submit handler fcn --> Receive input from search form and store it in a variable
+// Store search fcn --> store search in localStorage
+// Get current conditions fcn --> Fetch current conditions from api based on input, store it in variable(s)
+// Get future conditions fcn --> Fetch future conditions from api based on input, store it in variable(s)
+// Display current and future conditions --> display conditions function
+
+// Listen for click, then run form submiit handler fcn 
+    // --> store current search fcn, 
+    // --> get previous searches fcn, 
+    // --> display previous searches fcn, 
+    // --> get current conditions fcn 
+        // --> display current conditions fcn)
+    // --> Get future conditions fcn
+        // --> display future conditions fcn) 
