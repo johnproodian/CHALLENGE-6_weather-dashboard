85ad51c8921401da420b9072fa07fe2b741dc7a0
Jotas <jproodian721@gmail.com>
2021-12-15 22:22:18 -0600

    started on fetching current data--really meant for another branch...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+APIKey = "433db97a8512e8112426ca764b0710cc";
+
+var searchCity = "austin";
+
+// function to get current conditions
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var getCurrent = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    fetch(apiUrl)
+        .then(function(response) {
+            console.log("it fetched")
+            if(response.ok) {
+                console.log("response is good!");
+                response.json()
+                    .then()
+            } else {
+                alert("Error: didn't work...")
+            }
+
+    })
+}
+
+var getForecat = function(city) {
+    var apiUrl = "";
+}
+
+getCurrent(searchCity);
+
+
+
+
 // Get previous searches (local storage) fcn --> Check local storage and get previous searches
 // Display previous searches fcn --> Display previous searches in clickable buttons below search form
 
@@ -6,6 +40,14 @@
 // Form submit handler fcn --> Receive input from search form and store it in a variable
 // Store search fcn --> store search in localStorage
 // Get current conditions fcn --> Fetch current conditions from api based on input, store it in variable(s)
+    // conditions: 
+        // city name
+        // date
+        // icon rep of weather conditions
+        // temperature
+        // humidity
+        // wind speed
+        // UV index
 // Get future conditions fcn --> Fetch future conditions from api based on input, store it in variable(s)
 // Display current and future conditions --> display conditions function
 
