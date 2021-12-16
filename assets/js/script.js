d09b9ad3ad17bedcff7beb2c45a393f3717969e8
Jotas <jproodian721@gmail.com>
2021-12-16 09:24:20 -0600

    not totally sure--I forgot to commit something last night

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,8 +24,36 @@ var getCurrent = function(city) {
     })
 }
 
+// function to get and display current UVI (since current api didn't have it, I had to use the lat and lon values from current api to get the current uvi from the One Call API)
 var getCurrentUVI = function(latitude, longitude) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
     
+    // get api data AND display the UVI element
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+                    .then(function(data) {
+                        var currentUVI = data.current.uvi;
+                        var currentUVIEl = document.createElement("p");
+                        var currentUVISpan = document.createElement("span");
+                        currentUVISpan.textContent = currentUVI;
+                        if (currentUVI <= 2) {
+                            currentUVISpan.className = "bg-success text-white px-2 py-1";
+                        } else if (2 < currentUVI < 6) {
+                            currentUVISpan.className = "bg-warning px-2 py-1";
+                        } else {
+                            currentUVISpan.className = "bg-danger text-white px-2 py-1";
+                        }
+                        currentUVIEl.className = "ml-2";
+                        currentUVIEl.textContent = "UV Index: ";
+                        currentUVIEl.append(currentUVISpan); 
+                        currentEl.appendChild(currentUVIEl);
+                    })
+            } else {
+                alert("UV Index fetch failed");
+            }
+        })
 }
 
 var getForecast = function(city) {
@@ -43,19 +71,23 @@ var getForecast = function(city) {
         })
 }
 
+// function to display the data of the current weather from the api
 var displayCurrent = function(current) {
     // add city and date to top of div
     var cityName = current.name;
     var date = new Date();
     var [month, day, year] = [[date.getMonth() + 1], date.getDate(), date.getFullYear()];
-    var cityDateEl = document.createElement("h3");
+    var weatherIcon = current.weather.icon;
+    var cityDateIconEl = document.createElement("h3");
     date = "(" + month + "/" + day + "/" + year + ")";
-    cityDateEl.textContent = cityName + " " + date;
-    currentEl.appendChild(cityDateEl);
+    cityDateIconEl.className = "ml-2";
+    cityDateIconEl.textContent = cityName + " " + date + " " + weatherIcon;
+    currentEl.appendChild(cityDateIconEl);
 
     // add temp
     var temp = current.main.temp
     var tempEl = document.createElement("p");
+    tempEl.className = "ml-2";
     tempEl.textContent = "Temp: " + temp + "°F";
     currentEl.appendChild(tempEl);
     console.log("temp: " + temp);
@@ -63,17 +95,26 @@ var displayCurrent = function(current) {
     // add wind
     var wind = current.wind.speed;
     var windEl = document.createElement("p");
-    windEl.textContent = "Wind: " + wind + "MPH";
+    windEl.className = "ml-2";
+    windEl.textContent = "Wind: " + wind + " MPH";
     currentEl.appendChild(windEl);
+
+    // add humidity
+    var humidity = current.main.humidity;
+    var humidityEl = document.createElement("p");
+    humidityEl.className = "ml-2";
+    humidityEl.textContent = "Humidity: " + humidity + " %";
+    currentEl.appendChild(humidityEl);
 
 
     // add UV index
-    var uVIndex = "";
+    var lat = current.coord.lat;
+    var lon = current.coord.lon;
+    getCurrentUVI(lat, lon);
 
-    // h3 with [city] and [date]
-    // p with "Temp: " + temp data
-    // p with "Wind: " + wind data
-    // p with "UV Index: " + UV Data <-- this needs to be formatted with the right color...
+    // STILL NEEDED:
+        // change the color of the UV index depending on what it is
+        // 
 }
 
 getCurrent(searchCity);
